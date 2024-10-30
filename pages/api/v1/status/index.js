310d543a9c3f5d96eeb29778726c9bd480f79d2e
Victor Perotto Pioner <88547011+VictorPerotto@users.noreply.github.com>
2024-10-30 20:16:55 +0000

    Add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as Sum;");
   console.log(result.rows);
-  response.status(200).json({ descricao: "Funcionou de novo?" });
+  response.status(200).json({ descricao: "Funcionou" });
 }
 
 export default status;
